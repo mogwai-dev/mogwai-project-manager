50d74f4f7c2fe8cead979e56e9138a7c4fcebca4
yuma_noguchi <alchemy.lapidis.philosophorum.13@gmail.com>
2024-12-18 23:05:36 +0900

    react を追加

---
+++ src/App.tsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import tauriLogo from './assets/tauri.svg'
+import typescriptLogo from './assets/typescript.svg'
+import './App.css'
+import { invoke } from "@tauri-apps/api/core";
+
+
+async function greet() {
+  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+  alert(await invoke("greet", {
+    name: "ABC",
+  }));
+}
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a target="_blank">
+          <img src={tauriLogo} className="logo react" alt="React logo" />
+        </a>
+        <a target="_blank">
+          <img src={typescriptLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <button onClick={greet}>Greet</button>
+      <p id="greet-msg"></p>
+    </>
+  )
+}
+
+export default App
