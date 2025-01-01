fc8e7d20317ea130feaed01b865af30d2a6e7e97
yuma_noguchi <alchemy.lapidis.philosophorum.13@gmail.com>
2025-01-01 13:19:04 +0900

    テーブルの追加

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,32 @@ const TextFieldAndButtonPageComponent: React.FC = () => {
 const TablePageComponent: React.FC = () => {
   return (
     <div>
-      <p>sample</p>
+      <table className="table-fixed">
+        <thead>
+          <tr>
+            <th>Song</th>
+            <th>Artist</th>
+            <th>Year</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+            <td>Malcolm Lockyer</td>
+            <td>1961</td>
+          </tr>
+          <tr>
+            <td>Witchy Woman</td>
+            <td>The Eagles</td>
+            <td>1972</td>
+          </tr>
+          <tr>
+            <td>Shining Star</td>
+            <td>Earth, Wind, and Fire</td>
+            <td>1975</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
@@ -221,7 +246,8 @@ function App() {
         </ul>
       </div>{" "}
       <Page />
-      { /* 
+      {
+        /*
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
