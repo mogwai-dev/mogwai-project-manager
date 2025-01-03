60cd979a2df490696cec3e942ac6c31edca475c0
yuma_noguchi <alchemy.lapidis.philosophorum.13@gmail.com>
2025-01-03 16:01:13 +0900

    ボタンを押すとリストファイルにセーブする機能の追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import { open } from "@tauri-apps/plugin-dialog";
 import { invoke } from "@tauri-apps/api/core";
-import { readTextFile } from "@tauri-apps/plugin-fs";
+import { readTextFile, writeTextFile } from "@tauri-apps/plugin-fs";
 
 // 設定ディレクトリ内のファイルをすべて読み込む(*.list と *.table)
 async function get_list_file_names(settingDirPath: string): Promise<string[]> {
@@ -24,6 +24,15 @@ function create_text_field_and_button_page_component(
   path: string,
 ): React.FC {
   return () => {
+    const [textarea_value, set_textarea_value] = useState(text);
+    const [last_textarea_value, set_last_textarea_value] = useState(text); // 最後にセーブボタンを押された文字列
+
+    const has_difference = textarea_value !== last_textarea_value;
+
+    const class_when_has_diff = has_difference ? "border-orange-300" : "border-gray-300";
+    const button_class_when_has_diff = has_difference ? "bg-orange-500 hover:bg-orange-700" : "bg-gray-500"
+    
+
     return (
       <div className="mx-2">
         <label
@@ -35,13 +44,23 @@ function create_text_field_and_button_page_component(
         <textarea
           id="message"
           rows={4}
-          className="block my-2 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder={text}
-          defaultValue={text}
+          className={`block my-2 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border ${class_when_has_diff} focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+          placeholder={textarea_value}
+          value={textarea_value}
+          onChange={(e) => {
+            set_textarea_value(e.target.value);
+          }}
         >
         </textarea>
-        <button className="my-1 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded-full text-sm">
-          Button
+        <button 
+          disabled={!has_difference}
+          className={`my-1 ${button_class_when_has_diff} text-white font-bold py-1 px-2 rounded-full text-sm`}
+          onClick={async () => {
+            set_last_textarea_value(textarea_value);
+            await writeTextFile(path, textarea_value);
+          }}
+          >
+          save
         </button>
       </div>
     );
@@ -104,14 +123,7 @@ interface Tab {
 
 function App() {
   // 初期化関数内で useState を使用したいので、useState をあらかじめ使用しておく
-  const [tabs, setTabs] = useState<Tab[]>([
-    /*{
-      represent_name: "sample",
-      svg_path_d:
-        "M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z",
-      Page: create_text_field_and_button_page_component("sample"),
-    }*/
-  ]);
+  const [tabs, setTabs] = useState<Tab[]>([]);
 
   const [selectedTab, setSelectedTab] = useState(0);
 
